7baa9a05402c9c7bcf14fc0b47ef8456677f0033
eugene <eugene.rublenko@gmail.com>
2016-11-27 13:16:40 +0200

    implement minimize method in Batch class that does gradient descent

--- ml_toolbox/batch.py
+++ ml_toolbox/batch.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.optimize import minimize
 
 class Batch(object):
 	def __init__(self, func, grad, rate, num_iter):
@@ -6,6 +7,11 @@ class Batch(object):
 		self.gradient = grad
 		self.iterations = num_iter
 		self.learning_rate = rate
+	def minimize(self, initial_guess):
+		x = initial_guess
+		for i in range(self.iterations):
+			x = x - self.learning_rate*self.gradient(x)
+		return {'x_max': x, 'y_max': self.function(x)}
 
 
 if __name__ == '__main__':
