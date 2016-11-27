554bf077e2a0b3b41ca1e3e8a239c4c6f4bd5b00
eugene <eugene.rublenko@gmail.com>
2016-11-27 13:38:17 +0200

    add docstrings for Batch class

--- ml_toolbox/batch.py
+++ ml_toolbox/batch.py
@@ -2,7 +2,20 @@ import numpy as np
 from scipy.optimize import minimize
 
 class Batch(object):
+	"""
+	Batch class provides simple interface to gradient descent which is able to find local optimum.
+	The class has a constructor that takes user's function as callable which takes np.array as argument
+	and callable gradient of this function that returns gradient as np.array in certain point;
+	also, constructor takes learning rate and maximum number of iterations to converge.
+	Minimize method simply takes initial point to start descent and returns local optimum.
+	"""
 	def __init__(self, func, grad, rate, num_iter):
+		"""
+		func=your callable function that takes np.array and returns np.array;
+		grad=callable gradient to your function that takes np.array and returns np.array
+		rate=model parameter that defines learning rate
+		num_iter=maximum number of iterations
+		"""
 		self.function = func
 		self.gradient = grad
 		self.iterations = num_iter
