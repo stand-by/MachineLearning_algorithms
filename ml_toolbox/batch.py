2ec33c93d73d0537be42bf14310569eca01a5459
eugene <eugene.rublenko@gmail.com>
2016-11-27 12:01:22 +0200

    impelement constructor for Batch class

--- ml_toolbox/batch.py
+++ ml_toolbox/batch.py
@@ -1,7 +1,11 @@
 import numpy as np
 
 class Batch(object):
-	pass
+	def __init__(self, func, grad, rate, num_iter):
+		self.function = func
+		self.gradient = grad
+		self.iterations = num_iter
+		self.learning_rate = rate
 
 
 if __name__ == '__main__':
