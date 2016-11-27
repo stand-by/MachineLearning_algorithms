4d7fd4f12e9432f75ba4b3b01363d907e0dc934a
eugene <eugene.rublenko@gmail.com>
2016-11-27 17:31:24 +0200

    implement gradient of cost function

--- ml_toolbox/linear_regression.py
+++ ml_toolbox/linear_regression.py
@@ -1,12 +1,17 @@
+import gradient_descent
 import numpy as np
 
 class LinearRegression(object):
 	def __init__(self, data_table, answers, rate, max_iterations): #add tolerance parameter
 		self.X = data_table
 		self.y = answers
+		self.theta = None
 		self.learning_rate = rate
 		self.max_iters = max_iterations
 		self.m, self.n = data_table.shape
 	@staticmethod
 	def cost(X, y, theta):
 		return sum((np.dot(X,theta)-y)**2)/(2.0*X.shape[0])
+	@staticmethod
+	def cost_grad(X, y, theta):
+		return np.dot(X.T, (np.dot(X,theta)-y))/float(X.shape[0])
