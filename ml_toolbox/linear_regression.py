fdf48cb88c4a4a932b5286432513576f18796ce2
eugene <eugene.rublenko@gmail.com>
2016-11-27 16:46:37 +0200

    implement constructor in LinearRegression class

--- ml_toolbox/linear_regression.py
+++ ml_toolbox/linear_regression.py
@@ -1,4 +1,10 @@
 import numpy as np
 
 class LinearRegression(object):
-	pass
+	def __init__(self, data_table, answers, rate, max_iterations): #add tolerance parameter
+		self.X = data_table
+		self.y = answers
+		self.learning_rate = rate
+		self.max_iters = max_iterations
+		self.m, self.n = data_table.shape
+	
