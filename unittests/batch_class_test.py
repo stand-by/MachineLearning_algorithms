5f8dd94eefad0ce94e278527b3bc64303dfafd1b
eugene <eugene.rublenko@gmail.com>
2016-11-27 13:54:31 +0200

    add skeleton for Batch unit tests

---
+++ unittests/batch_class_test.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.insert(0, '../ml_toolbox')
+import gradient_descent
+import unittest
+import numpy as np
+from scipy.optimize import minimize
+
+class BatchTest(unittest.TestCase):
+	pass
+
+if __name__ == "__main__": 
+    unittest.main()
