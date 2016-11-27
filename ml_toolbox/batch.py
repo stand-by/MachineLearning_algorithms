8a525f5304a20775962006360f3f074fd721b0a0
eugene <eugene.rublenko@gmail.com>
2016-11-27 13:17:17 +0200

    add some initial test in batch.py

--- ml_toolbox/batch.py
+++ ml_toolbox/batch.py
@@ -15,4 +15,18 @@ class Batch(object):
 
 
 if __name__ == '__main__':
-	
+	gd = Batch((lambda x: (x+1)**2-3), (lambda x: 2*(x+1)), 0.1, 100)
+	print(gd.minimize(100.0))
+	print(minimize((lambda x: (x+1)**2-3), 100.0, jac=(lambda x: 2*(x+1))))
+	print("")
+
+	def f(x):
+		return (x[0]+2)**2+(x[1]-2)**2+10
+	def grad_f(x):
+		dx1 = 2*(x[0]+2)
+		dx2 = 2*(x[1]-2)
+		return np.array([dx1,dx2])
+	gd = Batch(f, grad_f, 0.1, 100)
+	print(gd.minimize(np.array([100.0,-10.0])))
+	print(minimize(f, np.array([100.0,-10.0]), jac=grad_f))
+	print("")
