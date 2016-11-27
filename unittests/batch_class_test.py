b59be40bd99df35e13c873e6212681a7228dd24e
eugene <eugene.rublenko@gmail.com>
2016-11-27 14:32:04 +0200

    add test case for Batch with single variable

--- unittests/batch_class_test.py
+++ unittests/batch_class_test.py
@@ -1,12 +1,21 @@
+import unittest
 import sys
 sys.path.insert(0, '../ml_toolbox')
 import gradient_descent
-import unittest
 import numpy as np
 from scipy.optimize import minimize
 
 class BatchTest(unittest.TestCase):
-	pass
+	eps = 0.00001
+	def test_minimize_singlevar(self):
+		gd = gradient_descent.Batch((lambda x: (x+1)**2-3), (lambda x: 2*(x+1)), 0.1, 100)
+		res_batch = gd.minimize(100.0)
+		print(res_batch)
+		res_scipy = minimize((lambda x: (x+1)**2-3), 100.0, jac=(lambda x: 2*(x+1)))
+		print(res_scipy)
+		self.assertTrue(np.abs(res_batch['y_max']-res_scipy.fun) < self.eps)
+		self.assertTrue(np.abs(res_batch['x_max']-res_scipy.x[0]) < self.eps)
+
 
 if __name__ == "__main__": 
     unittest.main()
