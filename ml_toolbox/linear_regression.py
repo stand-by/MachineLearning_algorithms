8b7751a2839b3301a9f70418a898af75a1bf0672
eugene <eugene.rublenko@gmail.com>
2016-11-30 07:31:18 +0200

    syntax hotfix in LinearRegression class

--- ml_toolbox/linear_regression.py
+++ ml_toolbox/linear_regression.py
@@ -2,24 +2,24 @@ import gradient_descent
 import numpy as np
 
 class LinearRegression(object):
-	def __init__(self, data_table, answers, rate, max_iterations): #add tolerance parameter
+	def __init__(self, data_table, answers, rate, max_iters): #add tolerance parameter
 		self.X = data_table
 		self.y = answers
 		self.theta = None
 		self.learning_rate = rate
-		self.max_iters = max_iterations
+		self.max_iterations = max_iters
 		self.m, self.n = data_table.shape
+		self.X = np.insert(self.X,0,np.ones(self.m),1)
 	def train_batch(self, inital_guess):
-		self.X = np.insert(self.X,0,np.ones(m),1)
 		J = lambda t: LinearRegression.cost(self.X,self.y,t)
 		J_grad = lambda t: LinearRegression.cost_grad(self.X,self.y,t)
-		batch = gradient_descent.Batch(J,J_grad,self.rate,self.max_iterations)
+		batch = gradient_descent.Batch(J,J_grad,self.learning_rate,self.max_iterations)
 		self.theta = batch.minimize(inital_guess)
 	def train_normal_equation(self):
-		self.theta = np.dot(np.dot(numpy.linalg.pinv(np.dot(np.transpose(self.X), self.X)), np.transpose(self.X)), self.y)
+		self.theta = np.dot(np.dot(np.linalg.pinv(np.dot(np.transpose(self.X), self.X)), np.transpose(self.X)), self.y)
 	def predict(self, x0):
 		x0 = np.insert(x0,0,1.0)
-		return x0*self.theta
+		return np.dot(x0,self.theta)
 	@staticmethod
 	def cost(X, y, theta):
 		return sum((np.dot(X,theta)-y)**2)/(2.0*X.shape[0])
