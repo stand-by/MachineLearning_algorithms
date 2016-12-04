6590bc412a18be8b629ef33fcb39652d77e3d6b9
eugene <eugene.rublenko@gmail.com>
2016-12-04 08:39:58 +0200

    add constructor for LogisticRegression

--- ml_toolbox/logistic_regression.py
+++ ml_toolbox/logistic_regression.py
@@ -2,4 +2,10 @@ import gradient_descent
 import numpy as np
 
 class LogisticRegression(object):
-	pass
+	def __init__(self, data_table, answers):
+		self.X = data_table
+		self.y = answers
+		self.theta = None
+		self.minimization_trace = None
+		self.X = np.insert(self.X,0,np.ones(self.m),1)
+		self.m, self.n = self.X.shape
