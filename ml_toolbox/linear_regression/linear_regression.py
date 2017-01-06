fc382d1506580cbe1a8db4d3fb6d7f3e1248f4e8
eugene <eugene.rublenko@gmail.com>
2017-01-06 12:48:25 +0200

    derive LinearRegression class from BasicRegression

--- ml_toolbox/linear_regression/linear_regression.py
+++ ml_toolbox/linear_regression/linear_regression.py
@@ -1,8 +1,9 @@
+from basic_regression import BasicRegression
 import gradient_descent
 import functools
 import numpy as np
 
-class LinearRegression(object):
+class LinearRegression(BasicRegression):
 	"""
 	LinearRegression class provides model for building predictions based on design matrix X and given answers y.
 	It has to be constructed with X matrix and y vector (class adds bias column automaticly).
@@ -15,13 +16,9 @@ class LinearRegression(object):
 		data_table=design matrix, without column of ones
 		answers=labels for data to train
 		"""
-		self.X = data_table
-		self.y = answers
-		self.theta = None
-		self.minimization_trace = None
-		self.m, self.n = self.X.shape
-		self.X = np.insert(self.X,0,np.ones(self.m),1)
-		self.n += 1
+		BasicRegression.__init__(self,data_table,answers)
+		self.add_intercept()
+
 	def train_batch(self, rate, tolerance, max_iters, inital_guess):
 		"""
 		rate=learning rate which will be used in gradient descent
@@ -34,18 +31,22 @@ class LinearRegression(object):
 		batch = gradient_descent.Batch(J,J_grad,rate,tolerance,max_iters)
 		self.minimization_trace = batch.minimize(inital_guess)
 		self.theta = self.minimization_trace[0]
+
 	def train_normal_equation(self):
 		"""
 		just solves normal equation and finds thethas (really slow for big design matrix)
 		"""
 		self.theta = np.dot(np.dot(np.linalg.pinv(np.dot(np.transpose(self.X), self.X)), np.transpose(self.X)), self.y)
+
 	def predict(self, x0):
 		"""
 		returns model's prediction for given vector x0 as argument
 		"""
 		x0 = np.insert(x0,0,1.0)
 		return np.dot(x0,self.theta)
+
 	def cost(self, X, y, theta):
 		return sum((np.dot(X,theta)-y)**2)/(2.0*X.shape[0])
+
 	def cost_grad(self, X, y, theta):
 		return np.dot(X.T, (np.dot(X,theta)-y))/float(X.shape[0])
