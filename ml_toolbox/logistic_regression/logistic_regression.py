b1a6258bcf461e8cd0f9f3df4fbf04b6a27c8f8f
eugene <eugene.rublenko@gmail.com>
2017-01-06 13:31:03 +0200

    remove cost and cost_grad from static methods in LogisticRegression

--- ml_toolbox/logistic_regression/logistic_regression.py
+++ ml_toolbox/logistic_regression/logistic_regression.py
@@ -1,6 +1,7 @@
 from basic_regression import BasicRegression
 import gradient_descent
 import numpy as np
+import functools
 from scipy import optimize
 
 def sigmoid(x):
@@ -12,33 +13,30 @@ class LogisticRegression(BasicRegression):
 		self.add_intercept()
 
 	def train_batch(self, rate, tolerance, max_iters, inital_guess):
-		J = lambda t: LogisticRegression.cost(self.X,self.y,t)
-		J_grad = lambda t: LogisticRegression.cost_grad(self.X,self.y,t)
+		J = functools.partial(self.cost,self.X,self.y)
+		J_grad = functools.partial(self.cost_grad,self.X,self.y)
 		batch = gradient_descent.Batch(J, J_grad, rate, tolerance, max_iters)
 		self.minimization_trace = batch.minimize(inital_guess)
 		self.theta = self.minimization_trace[0]
 
 	def train_nelder_mead(self, max_iters, inital_guess):
-		J = lambda t: LogisticRegression.cost(self.X,self.y,t)
-		J_grad = lambda t: LogisticRegression.cost_grad(self.X,self.y,t)
+		J = functools.partial(self.cost,self.X,self.y)
+		J_grad = functools.partial(self.cost_grad,self.X,self.y)
 		self.theta = optimize.fmin(J, x0=inital_guess, maxiter=max_iters, full_output=False, disp=False)
 
 	def probability(self, x0):
 		x0 = np.insert(x0,0,1.0)
-		return LogisticRegression.hypothesis(x0,self.theta)
+		return self.hypothesis(x0,self.theta)
 
 	def predict(self, x0):
 		klass = 1 if self.probability(x0) >= 0.5 else 0
 		return klass
 
-	@staticmethod
-	def hypothesis(X, theta):
+	def hypothesis(self, X, theta):
 		return sigmoid(np.dot(X,theta))
 
-	@staticmethod
-	def cost(X, y, theta):
-		return (np.dot(-y.T,np.log(LogisticRegression.hypothesis(X,theta)))-np.dot((1-y).T,np.log(1-LogisticRegression.hypothesis(X,theta))))/float(X.shape[0])
-		
-	@staticmethod
-	def cost_grad(X, y, theta):
-		return np.dot(X.T, (LogisticRegression.hypothesis(X,theta)-y))/float(X.shape[0])
+	def cost(self, X, y, theta):
+		return (np.dot(-y.T,np.log(sigmoid(np.dot(X,theta))))-np.dot((1-y).T,np.log(1-sigmoid(np.dot(X,theta)))))/float(X.shape[0])
+
+	def cost_grad(self, X, y, theta):
+		return np.dot(X.T, (sigmoid(np.dot(X,theta))-y))/float(X.shape[0])
