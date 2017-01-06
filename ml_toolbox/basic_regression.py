8bcd06f29d91f05bf545c129dd08f02c76a4636b
eugene <eugene.rublenko@gmail.com>
2017-01-06 11:02:55 +0200

    implement add_intercept method in BasicRegression

--- ml_toolbox/basic_regression.py
+++ ml_toolbox/basic_regression.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class BasicRegression(object):
     def __init__(self, data_table, answers):
     	self.X = data_table
@@ -5,3 +7,7 @@ class BasicRegression(object):
     	self.theta = None
     	self.minimization_trace = None
     	self.m, self.n = self.X.shape
+
+    def add_intercept(self):
+		self.X = np.insert(self.X,0,np.ones(self.m),1)
+		self.n += 1
